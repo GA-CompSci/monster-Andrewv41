9ac8720f1709c613f95da2ab3b444b476db37c45
Andrewv41 <andrew.varanese@gilmour.org>
2025-09-22 10:35:59 -0400

    jhjjhjhjhd

--- src/App.java
+++ src/App.java
@@ -1,5 +1,55 @@
+
+import java.util.Scanner;
 public class App {
+    private static Monster [];
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("--- MONSTER BATTLE ---");
+
+        Monster m1 = new Monster();
+        Monster m2 = new Monster();
+
+        System.out.println("M1 health : " + m1.health());
+        System.out.println("M2 health : " + m2.health());
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many monsters will you slay : ");
+        int num = input.nextInt();
+        Monster[] monsters = new Monster[num];
+        //build all monsters
+        for(int i = 0; i < monsters.length; i++){
+            monsters[i] = new Monster();  //TODO add specials
+        }
+        //GAME LOOP
+       int count = 0;
+
+       for(Monster m : monsters){
+        if(m.health() > 50) count ++;
+       }
+    //   for(int i = 0; i < monste.length; i++){
+    //    if (monster[i].health >= 50) count++;
+    //   }
+       System.out.println(count + " monsters have more than 50 healt");
+
+
     }
-}
+    /**
+     * How many monsters have over the given health
+     * @param health
+     * @param arr
+     * @return
+     */
+    public static int monsterCount(int health, Monster[]){
+        int count1 = 0;
+        for(Monster m : arr){
+            if(m.health() > health) count1++;
+        }
+        return count1;
+        }
+
+        public static Monster getNextMonster(){
+            if(Monster[i].health()>0) return Monster[i];
+        }
+    }
+
+
---
+++ src/Monster.java
@@ -0,0 +1,29 @@
+public class Monster {
+    // INSTANCE VARIABLES (properties / data variables)
+    private int health;
+    private double damage;
+    private int speed;
+    private String special;
+
+    //CONSTRUCTOR
+    public Monster(){
+        // randomly generate health, damage, speed
+        health = (int)(Math.random()*80 + 1) + 20;
+        // random 10 - 50
+        damage = (double)(Math.random()*41) + 10;
+        //random 1 - 10
+        speed = (int)(Math.random()*10) + 1;
+        //by default the monster has no special move
+        special = "";
+}
+    //OVERLOADED CONSTRUCTOR
+    public Monster(String special){
+        this();
+        this.special = special;
+    }
+    //ACCESSOR METHODS
+    public int health(){return health; }
+    public double damage() {return damage;}
+    public int speed() {return speed;}
+    public String special(){return special;}
+}
